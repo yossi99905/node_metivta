41d892889290d48db519508e4cca410b471d7d6c
yossi mizrachi <yossimizrach.770@gmail.com>
2024-05-08 22:20:59 +0300

    Refactor classRoom filter in TeacherController to handle classNum 0 as a special case

--- controllers/teacerControllers.js
+++ controllers/teacerControllers.js
@@ -10,13 +10,14 @@ TeacherController = {
         const classNum = req.query.classNum;
         try {
             let query = {};
-            if (classNum !== "0") {
+            if (classNum !== 0) {
                 query.classRoom = classNum;
             }
             
+            
             const data = await UserModel.find({
                 ...query,// if classNum is not 0 then add classNum to the query
-                role: { $in: [1000] } // 1000 is the role of the student
+                role: { $in: ["1000"] } // 1000 is the role of the student
             }).select('lastName firstName email classRoom score');
             res.json(data);
         }
