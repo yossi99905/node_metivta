1353300f8849536b78a70c79ca9538a11749cc28
yossi mizrachi <yossimizrach.770@gmail.com>
2024-05-23 11:00:51 +0300

    chore: Update authentication middleware to handle additional role values

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ const UserController = require('../controllers/userControllers')
 const express = require("express");
 const router = express.Router();
 
-const { authTeacher, authStudent,authAdmin } = require('../middleware/auth')
+const { authTeacher, authStudent,authAdmin,authStore } = require('../middleware/auth')
 
 
 
-router.get("/", authAdmin, UserController.getAllUsers);
+router.get("/", authStore, UserController.getAllUsers);
 router.post("/",authAdmin ,UserController.createUser);
 router.put("/:id",authAdmin,UserController.updateUser);
 router.delete("/:id",authAdmin,UserController.deleteUser);
